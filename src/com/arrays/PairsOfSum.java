c14be29f30eac9616baf66427c1e92f7de53cc61
srivatsav <srivatsav.gorti@phenompeople.com>
2020-04-29 19:50:14 +0530

    Finding Pairs of a target

---
+++ src/com/arrays/PairsOfSum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PairsOfSum {
+
+  public static void main(String[] args) {
+
+    int[] arr = { 1, 2, 3, 4, 3, 5, 0, 6 };
+    int target = 6;
+
+    findIndexOfPairsForTarget(arr, target);
+  }
+
+  static void findIndexOfPairsForTarget(int[] arr, int target) {
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    for (int i = 0; i < arr.length; i++) {
+
+      if (map.containsKey(arr[i])) {
+        System.out.println("Pair found at :: " + map.get(arr[i]) + " and " + i);
+      } else {
+        map.put(target - arr[i], i);
+      }
+
+    }
+  }
+}
